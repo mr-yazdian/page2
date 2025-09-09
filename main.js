e34e65bea4fac90efdec9b45651bcce533736b92
mr-yazdian <mamaddddd184@gmail.com>
2025-09-09 04:22:12 -0800

    yazdian-code

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let span =document.querySelector("div.lanech>div.body>div.Segment>span")
-span.addEventListener("click" (){
-    
-})
+let spans = document.querySelectorAll("span.bold");
+
+spans.forEach(span => {
+  span.addEventListener("click", () => {
+    span.classList.toggle("blcak-bold");
+  });
+});
+
+
